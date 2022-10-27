ee8047cbfbe040514bb8e77fa7018a5887812d4f
brycches <brycches@byui.edu>
2022-10-27 13:46:11 -0600

    finished buttons for two step login?

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage,TextInput, Button, Alert } from 'react-native';
 import  Navigation from './components/Navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
@@ -13,8 +13,12 @@ const AppStack = createNativeStackNavigator();
 
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [phoneNumber,setPhoneNumber] = React.useState("");
   const [isLoggedIn,setIsLoggedIn] = React.useState(false);
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [tempCode, setTempCode]= React.useState(null)
+
+
 
    if (isFirstLaunch == true){
 return(
@@ -23,46 +27,108 @@ return(
 );
   }else if(isLoggedIn){
     return <Navigation/>
-  } else {
-    return(
+  } else{
+    return (
       <View>
-        <TextInput
-        style={StyleSheet.input}
-        placeholderTextColor='#4251f5'
-        placeholder='Cell Phone'>
+        <TextInput 
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          style={styles.input}  
+          placeholderTextColor='#4251f5' 
+          placeholder='Cell Phone'>          
         </TextInput>
         <Button
           title='Send'
           style={styles.button}
-          onpress={()=>{
+          onPress={async()=>{
             console.log('Button was pressed')
+
+            await fetch(
+              'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
+              {
+
+                method: 'POST',
+                headers:{
+                  'content-type' : 'application/text'
+                }
+
+              }
+            )
           }}
-        />
+        />        
+        <TextInput 
+          value={tempCode}
+          onChangeText={setTempCode}
+          style={styles.input2}  
+          placeholderTextColor='#4251f5' 
+          placeholder='Enter Code'>          
+        </TextInput>
+        <Button
+          title='Verify'
+          style={styles.button}
+          onPress={async()=>{
+            console.log('Button 2 was pressed')
+
+            const loginResponse=await fetch(
+              'https://dev.stedi.me/twofactorlogin',
+              {
+
+                method: 'POST',
+                headers:{
+                  'content-type' : 'application/text'
+                },
+                body:JSON.stringify({
+                  phoneNumber,
+                  oneTimePassword:tempCode
+                })
+
+              }
+            )
+            console.log("status", loginResponse.status)
+
+            if(loginResponse.status == 200){
+              const sessionToken = await loginResponse.text();
+              console.log('Session Token', sessionToken)
+              setIsLoggedIn(true);
+            }
+            else{
+              Alert.alert('Warning', 'An invalid Code was entered')
+            }
+          }}
+        /> 
       </View>
     )
   }
 }
  export default App;
 
+ 
  const styles = StyleSheet.create({
-  container:{
-      flex:1, 
-      alignItems:'center',
-      justifyContent: 'center'
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    marginTop:350,
-  },
-  margin:{
-    marginTop:100
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  }    
-})
+     container:{
+         flex:1, 
+         alignItems:'center',
+         justifyContent: 'center'
+     },
+     input: {
+       height: 40,
+       margin: 12,
+       borderWidth: 1,
+       padding: 10,
+       marginTop: 250
+     },
+     input2: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      marginTop:50
+    },
+     margin:{
+       marginTop:100
+     },
+     button: {
+       alignItems: "center",
+       backgroundColor: "#DDDDDD",
+       padding: 10
+     }    
+ })
