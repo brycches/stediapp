dd732831a51523ec9262cdc105c5373b24f808d5
brycches <brycches@byui.edu>
2022-11-10 13:42:28 -0700

    add a camera

--- App.js
+++ App.js
@@ -27,6 +27,7 @@ const App = () =>{
 
       if(validateResponse.status == 200){
         const userEmail = await validateResponse.text();
+        await AsyncStorage.setItem('userName',userEmail);
         console.log('userEmail', userEmail);
         setIsLoggedIn(true);
       }
@@ -57,7 +58,7 @@ return(
           onPress={async()=>{
             console.log('Button was pressed')
 
-            await fetch(
+            const sendTextResponse=await fetch(
               'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
               {
 
@@ -68,6 +69,10 @@ return(
 
               }
             )
+            if (sendTextResponse.status !=200){
+              console.log('Server send text response: '+sendTextResponse.status);
+              Alert('Communication Error','Server responded to send text with status: '+sendTextResponse.status);
+            }
           }}
         />        
         <TextInput 
@@ -111,8 +116,9 @@ return(
               setIsLoggedIn(true);
             }
             else{
-              console.log("token response Status", loginRespone.status)
+              console.log("token response Status", loginResponse.status)
               Alert.alert('Warning', 'An invalid Code was entered')
+              setIsLoggedIn(false);
             }
           }}
         /> 
